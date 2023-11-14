012d029d902a6912f1de46857a368099f49e93d4
fabioarilete <fabioarilete@hotmail.com>
2023-11-14 16:33:20 -0300

    feat: finished

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import Dashboard from './scenes/dashboard/Dashboard';
 import Team from './scenes/team/Team';
 import Contacts from './scenes/contacts/Contacts';
 import Invoices from './scenes/invoices/Invoices';
-// import Bar from './scenes/Bar/bar';
-// import Form from './scenes/Form/form';
-// import Line from './scenes/Line/line';
-// import Pie from './scenes/Pie/pie';
-// import FAQ from './scenes/FAQ/faq';
-// import Geography from './scenes/Geography/geography';
-// import Calendar from './scenes/Calendar/calendar';
+import Form from './scenes/form/Form';
+import Calendar from './scenes/calendar/Calendar';
+import FAQ from './scenes/faq/FAQ';
+import Bar from './scenes/bar/Bar';
+import Pie from './scenes/pie/Pie';
+import Line from './scenes/line/Line';
+import Geography from './scenes/geography/Geography';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -30,13 +30,13 @@ function App() {
             <Route path="/team" element={<Team />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/invoices" element={<Invoices />} />
-            {/* <Route path="/form" element={<Form />} /> */}
-            {/* <Route path="/bar" element={<Bar />} /> */}
-            {/* <Route path="/pie" element={<Pie />} /> */}
-            {/* <Route path="/line" element={<Line />} /> */}
-            {/* <Route path="/faq" element={<FAQ />} /> */}
-            {/* <Route path="/geography" element={<Geography />} /> */}
-            {/* <Route path="/calendar" element={<Calendar />} /> */}
+            <Route path="/form" element={<Form />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/bar" element={<Bar />} />
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/line" element={<Line />} />
+            <Route path="/geography" element={<Geography />} />
           </Routes>
         </main>
       </div>
